973872aafe910add0c731ea34cc3170a5a2c3be1
Calinnor <58951773+Calinnor@users.noreply.github.com>
2021-01-09 12:58:19 +0100

    Initial commit : add 3 activity, implement StartActivity and redirection to LoginActivity

---
+++ app/src/main/java/com/cirederf/go4lunch/StartActivity.java
@@ -0,0 +1,31 @@
+package com.cirederf.go4lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        this.startCorrectActivity();
+    }
+
+    private void startCorrectActivity() {
+        this.startLogin();
+        //TODO: after isCurrentUserLogged implement, add here if logged or not and add startMain
+    }
+
+    private void startLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void startMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
