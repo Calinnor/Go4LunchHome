a976ecbefb9ecfddad0e00c787910f3222a28a13
Calinnor <frederic.hautson@gmail.com>
2021-01-14 15:49:57 +0100

    Add BottomNavigation drawer

--- app/src/main/java/com/cirederf/go4lunch/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/cirederf/go4lunch/Controllers/Activities/MainActivity.java
@@ -7,10 +7,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Activity;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,22 +24,21 @@ import com.cirederf.go4lunch.Controllers.Fragments.YourLunchFragment;
 import com.cirederf.go4lunch.R;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Locale;
-
 import butterknife.BindView;
 
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    @BindView(R.id.activity_main_toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.activity_main_drawer_layout)
-    DrawerLayout drawerLayout;
-    @BindView(R.id.activity_main_nav_view)
-    NavigationView navigationView;
+      @BindView(R.id.activity_main_toolbar)
+      Toolbar toolbar;
+      @BindView(R.id.activity_main_drawer_layout)
+      DrawerLayout drawerLayout;
+      @BindView(R.id.activity_main_nav_view)
+      NavigationView navigationView;
 
     private Fragment yourLunchFragment;
 
@@ -50,6 +46,11 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private static final int FRAGMENT_SETTINGS = 1;
     private static final int FRAGMENT_YOUR_LUNCH = 2;
     private static final int SIGN_OUT_TASK = 10;
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView mBottomNavigationView;
+    @BindView(R.id.content_name)
+    TextView tvContenName;
 
     @Override
     public int getActivityLayout() {
@@ -59,108 +60,28 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     @Override
     protected void onResume() {
         super.onResume();
+        this.configureBottomNavigationView();
         this.configureToolbar();
         this.configureNavigationDrawer();
         this.configureDrawerLayout();
-        this.showDefaultFragment();
+      //  this.showDefaultFragment();
         this.updateUi();
     }
 
-    // Actions
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id){
-            case R.id.activity_main_drawer_settings:
-                this.showItemMenuAction(FRAGMENT_SETTINGS);
-                break;
-            case R.id.activity_main_drawer_your_lunch:
-                this.showItemMenuAction(FRAGMENT_YOUR_LUNCH);
-                break;
-            case R.id.activity_main_drawer_logout:
-                this.showItemMenuAction(ITEM_MENU_LOGOUT);
-                break;
-            default:
-                break;
-        }
-        this.drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
-    @Override
-    public void onBackPressed() {
-        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            this.drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+    /**
+     *  FIREBASE REQUESTS
+     *  GetInstance
+     *  Get data
+     *  Logout
+     *  UpdateUi
+     */
 
-    //configuration toolbar, drawerLayout and navigationDrawer
-    protected void configureToolbar(){
-        setSupportActionBar(toolbar);
-    }
-
-    private void configureDrawerLayout(){
-        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toogle);
-        toogle.syncState();
-    }
-
-    private void configureNavigationDrawer(){
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    // Configure item menu views as fragments views and a first fragment view
-    private void startTransactionFragment(Fragment fragment) {
-        if (!fragment.isVisible()) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.activity_main_frame_layout, fragment).commit();
-        }
-    }
-
-    private void showYourLunchFragment(){
-        if (this.yourLunchFragment == null) this.yourLunchFragment = YourLunchFragment.newInstance();
-        this.startTransactionFragment(this.yourLunchFragment);
-    }
-
-    private void showItemMenuAction(int fragmentIdentifier){
-        switch (fragmentIdentifier){
-            case ITEM_MENU_LOGOUT:
-                this.onClickLogoutButton();
-                break;
-            case FRAGMENT_SETTINGS:
-                break;
-            case FRAGMENT_YOUR_LUNCH:
-                this.showYourLunchFragment();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void showDefaultFragment(){
-        Fragment visibleFragment = getSupportFragmentManager().findFragmentById(R.id.activity_main_frame_layout);
-        if (visibleFragment == null){
-            this.showItemMenuAction(FRAGMENT_YOUR_LUNCH);
-            this.navigationView.getMenu().getItem(0).setChecked(true);
-        }
-    }
-
+    //Get instance
     @Nullable
     protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
 
-    private void updateUi(){
-        NavigationView navigationView = findViewById(R.id.activity_main_nav_view);
-        View headerView = navigationView.getHeaderView(0);
-        TextView navUsername = headerView.findViewById(R.id.nav_header_username_txt);
-        TextView navUserMail = headerView.findViewById(R.id.nav_header_user_email_txt);
-        ImageView navUserPicture = headerView.findViewById(R.id.nav_header_user_picture);
-        this.updateUsername(navUsername);
-        this.updateUserMail(navUserMail);
-        this.updateUserPicture(navUserPicture);
-    }
-
+    //Get data
     private void updateUsername(TextView textViewUsernameToUpdate){
         String username = TextUtils.isEmpty(this.getCurrentUser().getDisplayName()) ? getString(R.string.info_no_username_found) : this.getCurrentUser().getDisplayName();
         textViewUsernameToUpdate.setText(username);
@@ -178,7 +99,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
                 .into(imageViewUserPictureToUpdate);
     }
 
-   //Logout actions :http request, close activity, show dialog to confirm option
+    //Logout
     private void signOutUserFromFirebase(){
         AuthUI.getInstance()
                 .signOut(this)
@@ -200,6 +121,27 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         };
     }
 
+    //Update UI
+    private void updateUi(){
+        NavigationView navigationView = findViewById(R.id.activity_main_nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = headerView.findViewById(R.id.nav_header_username_txt);
+        TextView navUserMail = headerView.findViewById(R.id.nav_header_user_email_txt);
+        ImageView navUserPicture = headerView.findViewById(R.id.nav_header_user_picture);
+        this.updateUsername(navUsername);
+        this.updateUserMail(navUserMail);
+        this.updateUserPicture(navUserPicture);
+    }
+
+
+    /**
+     * ACTIONS
+     * LogoutClick
+     * BottomNavigation
+     * DrawerNavigation
+     * 
+     */
+    //Logout
     public void onClickLogoutButton() {
         new AlertDialog.Builder(this)
                 .setMessage(R.string.popup_message_confirmation_logout_account)
@@ -212,5 +154,103 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
                 .setNegativeButton(R.string.popup_message_choice_no, null)
                 .show();
     }
+    
+    //BottomNavigation
+    private void configureBottomNavigationView() {
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {//new bottomnavigationview with setOn
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        tvContenName.setText(R.string.bottom_navigation_menu_map);
+                        return true;
+                    case R.id.chat:
+                        tvContenName.setText(R.string.bottom_navigation_menu_list);
+                        return true;
+                    case R.id.profil:
+                        tvContenName.setText(R.string.bottom_navigation_menu_workmate);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    //DrawerNavigation 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.activity_main_drawer_settings:
+                this.showItemMenuAction(FRAGMENT_SETTINGS);
+                break;
+            case R.id.activity_main_drawer_your_lunch:
+                this.showItemMenuAction(FRAGMENT_YOUR_LUNCH);
+                break;
+            case R.id.activity_main_drawer_logout:
+                this.showItemMenuAction(ITEM_MENU_LOGOUT);
+                break;
+            default:
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void showItemMenuAction(int fragmentIdentifier) {
+        switch (fragmentIdentifier) {
+            case ITEM_MENU_LOGOUT:
+                this.onClickLogoutButton();
+                break;
+            case FRAGMENT_SETTINGS:
+                break;
+            case FRAGMENT_YOUR_LUNCH:
+                this.showYourLunchFragment();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * CONFIGURATION
+     */    
+    protected void configureToolbar(){
+        setSupportActionBar(toolbar);
+    }
+
+    private void configureDrawerLayout(){
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+    }
+
+    private void configureNavigationDrawer(){
+        navigationView.setNavigationItemSelectedListener(this);//with setNavigation
+    }
+    //TODO find the difference between set and setOnNavigation and try to merge the two method similare but not equals
+
+    // Configure item menu views as fragments views and a first fragment view
+    private void startTransactionFragment(Fragment fragment) {
+        if (!fragment.isVisible()) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_content, fragment).commit();
+        }
+    }
+
+    private void showYourLunchFragment(){
+        if (this.yourLunchFragment == null) this.yourLunchFragment = YourLunchFragment.newInstance();
+        this.startTransactionFragment(this.yourLunchFragment);
+    }
+
 
 }
